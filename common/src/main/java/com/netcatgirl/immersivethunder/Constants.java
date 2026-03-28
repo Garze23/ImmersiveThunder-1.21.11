e5fb391d67b661185d00f0fd24112c5ea12d1f85
”didanko228” <51835948+didanko228@users.noreply.github.com>
2026-03-28 15:01:33 +0300

    port to 26.1

--- common/src/main/java/com/netcatgirl/immersivethunder/Constants.java
+++ common/src/main/java/com/netcatgirl/immersivethunder/Constants.java
@@ -1,6 +1,6 @@
 package com.netcatgirl.immersivethunder;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +11,12 @@ public class Constants {
     public static final String MOD_NAME = "Immersive Thunder";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
-    public static final ResourceLocation THUNDER_CLOSE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "thunder_close");
+    public static final Identifier THUNDER_CLOSE = Identifier.fromNamespaceAndPath(MOD_ID, "thunder_close");
     public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_CLOSE = SoundEvent.createVariableRangeEvent(THUNDER_CLOSE);
 
-    public static final ResourceLocation THUNDER_MEDIUM = ResourceLocation.fromNamespaceAndPath(MOD_ID, "thunder_medium");
+    public static final Identifier THUNDER_MEDIUM = Identifier.fromNamespaceAndPath(MOD_ID, "thunder_medium");
     public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_MEDIUM = SoundEvent.createVariableRangeEvent(THUNDER_MEDIUM);
 
-    public static final ResourceLocation THUNDER_FAR = ResourceLocation.fromNamespaceAndPath(MOD_ID, "thunder_far");
+    public static final Identifier THUNDER_FAR = Identifier.fromNamespaceAndPath(MOD_ID, "thunder_far");
     public static SoundEvent ENTITY_LIGHTNING_BOLT_THUNDER_FAR = SoundEvent.createVariableRangeEvent(THUNDER_FAR);
 }
